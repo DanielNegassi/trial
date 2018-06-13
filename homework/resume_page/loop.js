ecc1b3a547e2829139bb5002ba1f3275fb3ab04b
Daniel Negassi <dyn29@hotmail.com>
2018-06-13 07:51:20 -0500

    Commit 9 - Created loops.js file

---
+++ homework/resume_page/loop.js
@@ -0,0 +1,31 @@
+for (let x=0; x<100; x++){
+	console.log(x);
+}
+
+for (let y=99; y>=0; y--){
+	console.log(y);
+}
+
+for(z=0; z<=98; z++)
+if ((z%2)===0){
+	console.log(z);
+}
+
+for(p=98; p>=0; p--)
+if ((p%2)===0){
+	console.log(p);
+}
+
+for (let d=49; d<=72; d++){
+	console.log(d);
+}
+
+for (let s=81; s>=26; s--){
+	console.log(s);
+}
+
+for(l=3; l<=90; l++)
+if ((l%3)===0){
+	console.log(l);
+}
+
